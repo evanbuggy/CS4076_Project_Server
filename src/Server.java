8835304585f24677b70adbf91421f5a91ce35c8c
Buggy <124364531+evanbuggy@users.noreply.github.com>
2024-03-14 13:02:13 +0000

    Data retention for client disconnect

--- src/Server.java
+++ src/Server.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 public class Server {
     private static final int PORT = 9999;
     private static ServerSocket sock;
+    boolean exiting = false;
 
     public static void main(String[] args) {
         System.out.println("Opening port...\n");
@@ -18,27 +19,21 @@ public class Server {
             System.exit(1);
         }
 
-        do {
-            run();
-        } while (true);
-
-    }
-
-    private static void run() {
-
         Input command = new Input();
-        Socket link = null;
 
-        try {
-            System.out.println("Waiting for connection...");
-            link = sock.accept();
-            System.out.println("Connection established with " + link.getInetAddress());
-            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
-            System.out.println("InputStream established");
-            PrintWriter out = new PrintWriter(link.getOutputStream(), true);
-            System.out.println("OutputStream established");
+        do {
 
-            while (true) {
+            Socket link = null;
+
+            try {
+                System.out.println("Waiting for connection...");
+                link = sock.accept();
+                System.out.println("Connection established with " + link.getInetAddress());
+                BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+                System.out.println("InputStream established");
+                PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+                System.out.println("OutputStream established");
+
                 String message = "";
                 System.out.println("Waiting to read user input stream...");
 
@@ -46,7 +41,7 @@ public class Server {
                     message = in.readLine();
                 }
                 catch (SocketException e) {
-                    System.out.println("Client has abruptly exited!");
+                    System.out.println("Client has abruptly exited/failed to pass an output stream!");
                     break;
                 }
 
@@ -55,22 +50,23 @@ public class Server {
                 out.println(response);
                 System.out.println("Response: " + response);
                 if (Objects.equals(message, "TERMINATE")) {
+                    System.out.println("The client has requested to end communication.");
                     break;
                 }
             }
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                System.out.println("\n*** DISCONNECTING... ***");
-                link.close();
+            catch(IOException e) {
+                e.printStackTrace();
             }
-            catch (IOException e) {
-                System.out.println("\n*** UNABLE TO DISCONNECT, EXITING... ***");
-                System.exit(1);
+            finally {
+                try {
+                    System.out.println("\n*** DISCONNECTING... ***");
+                    link.close();
+                }
+                catch (IOException e) {
+                    System.out.println("\n*** UNABLE TO DISCONNECT, EXITING... ***");
+                    System.exit(1);
+                }
             }
-        }
+        } while (true);
     }
 }
